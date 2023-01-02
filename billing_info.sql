3ff5da026aa4745e4edaa9f1a720f37285453bed
Bhawesh02 <bhaweshagarwal02@gmail.com>
2023-01-02 19:58:01 +0530

    Update billing_info.sql

--- billing_info.sql
+++ billing_info.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 02, 2023 at 09:47 AM
+-- Generation Time: Jan 02, 2023 at 03:27 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -20,6 +20,39 @@ SET time_zone = "+00:00";
 --
 -- Database: `billing info`
 --
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `recalculate_id` ()   Begin
+	set @row_num = 0;
+    UPDATE product set product_id = (@row_num:=@row_num+1);
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `customer_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phone_number` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `users_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`customer_id`, `name`, `phone_number`, `email`, `users_id`) VALUES
+(1, 'c1', 465, 'asd@asd.com', 1),
+(2, 'Megha Agarwa', 123, 'as@sad.c', 1);
 
 -- --------------------------------------------------------
 
@@ -39,7 +72,11 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`product_id`, `name`, `price`, `users_id`) VALUES
-(1, 'Soap', '10.00', 1);
+(1, 'wqew', '1231.00', 1),
+(2, 'Casd', '50000.00', 1),
+(3, 'asd', '12.00', 1),
+(4, 'hjg', '12.00', 1),
+(5, '123', '123.00', 1);
 
 -- --------------------------------------------------------
 
@@ -67,6 +104,13 @@ INSERT INTO `users` (`users_id`, `name`, `email`, `PASSWORD`) VALUES
 --
 
 --
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customer_id`),
+  ADD KEY `users_id` (`users_id`);
+
+--
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
@@ -84,10 +128,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -98,6 +148,12 @@ ALTER TABLE `users`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `customer`
+--
+ALTER TABLE `customer`
+  ADD CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`users_id`);
 
 --
 -- Constraints for table `product`
