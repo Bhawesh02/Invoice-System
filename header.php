120ba9112407efec3470efd07c7258bcdad3640f
Bhawesh02 <bhaweshagarwal02@gmail.com>
2023-01-02 18:46:12 +0530

    product page done

--- header.php
+++ header.php
@@ -1,3 +1,10 @@
+<head><link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+      rel="stylesheet"
+    />
+    <script src="https://kit.fontawesome.com/36bed4b74a.js" crossorigin="anonymous"></script></head>
 <header>
       <div class="inner flex">
         <div class="logo">
--- home.php
+++ home.php
@@ -6,14 +6,9 @@ include 'Sesson_end.php';
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
-      rel="stylesheet"
-    />
-    <script src="https://kit.fontawesome.com/36bed4b74a.js" crossorigin="anonymous"></script>
+    
     <link rel="stylesheet" type="text/css" href="./css/home.css" />
+    <link rel="stylesheet" type="text/css" href="./css/header.css" />
     <title>Homme page</title>
   </head>
   <body>
